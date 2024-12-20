e7080b4aa057a78a08f1c8e6fc3d2d19fa79b8fb
Bhoumik <Bhoumikchopra12@gmail.com>
2024-12-20 06:21:00 +0530

    cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Use the CORS middleware with the configured options
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json()); // Parse incoming JSON data (important for POST requests)
 
 app.use(authRouter);
